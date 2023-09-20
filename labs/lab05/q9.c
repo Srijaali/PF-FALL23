286cb520e5e247aab8ae6629e551a7b1068272c5
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-09-20 22:20:36 +0500

    Create q9.c

---
+++ labs/lab05/q9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()  {
+               float a,b,c;
+               printf("Enter a: ");
+               scanf("%f", &a);
+               printf("Enter b: ");
+               scanf("%f", &b);
+               printf("Enter c: ");
+               scanf("%f", &c);
+
+                float sqroot;
+                float discriminant = (b*b)-(4*a*c);
+                if(discriminant < 0) {
+                    printf("Imaginary roots \n");
+                }
+                else if(discriminant > 0) {
+                    sqroot=sqrt(discriminant);
+                    float x1 =((-1*b)+(sqroot))/(2*a);
+                    float x2 =((-1*b)-(sqroot))/(2*a);
+                     printf(" The roots are Distinct and Real \n");
+                }else if(discriminant == 0) {
+                    printf("Real Roots \n");
+                }
+                return 0;
+
+
+
+}
