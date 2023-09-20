1b8cd5b4e45eaa02581b111a041e8824a2b5d515
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-09-21 00:52:48 +0500

    Create q6.c

---
+++ labs/lab05/q6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+            int num;
+            int bitnum7;
+            int bitnum4;
+            printf("Enter 8 bit number; ");
+            scanf("%d", &num);
+
+            bitnum7 = (num / 1000000) % 10;
+            bitnum4 = (num / 1000) % 10;
+
+            if(bitnum7 && bitnum4) {
+                num = num - 1000000;
+                num = num - 1000;
+            } 
+               printf("%d", num);
+
+                return 0;
+
+} //end main 
