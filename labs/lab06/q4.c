d8d8ca86c40e26652aeead7835a79598ed42f0d2
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-10-03 11:09:48 +0500

    Create q4.c

---
+++ labs/lab06/q4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+	int a,b;
+	printf("Enter two numbers: ");
+	scanf("%d %d", &a, &b);
+
+	if (a<=9) {
+	for (int k = a; k <= b; ++k)
+	{ 
+	switch(k) {
+
+	case 0: printf("Zero "); break;
+	case 1: printf("One "); break;
+	case 2: printf("Two "); break;
+	case 3: printf("Three "); break;
+	case 4: printf("Four "); break;
+	case 5: printf("Five "); break;
+	case 6: printf("Six "); break;
+	case 7: printf("Seven "); break;
+	case 8: printf("Eight"); break;
+	case 9: printf("Nine"); break;
+	default: if(k%2==0) printf(" it is Even ");
+		else printf("it is Odd ");
+
+
+
+}
+}
