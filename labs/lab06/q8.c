ff08a5f74bf1f5f15de914067ac9fab6892532e8
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-10-10 00:06:22 +0500

    Create q8.c

---
+++ labs/lab06/q8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+            int arrOfnums[] = {1,2,3,4,5,6,7,8,9};
+            int i,reverse,numbers;
+
+                for(i=0; i<9; i++){
+                  numbers= arrOfnums[i]; 
+                
+                printf("%d\n", numbers);
+                }
+
+            for(i=8; i>=0; i--){
+                reverse = arrOfnums[i];
+                printf("\nthe reverse is: ");
+                printf("%d\n", reverse);
+            }
+            return 0; 
+
+
+
+}
