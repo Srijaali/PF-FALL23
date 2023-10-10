a716eeed7fc65f81e63bd0fd6e72637ae1b58dbc
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-10-10 20:46:53 +0500

    Create q9.c

---
+++ labs/lab06/q9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+int arr[] = {4,1,6,8,10,21,8,9,2,6};
+int i,j,min,max;
+
+    
+min = arr[0];
+max = arr[0];
+
+
+    for(i=0; i<10; i++){
+    if(max < arr[i])
+        max = arr[i];
+    else if(min > arr[i])
+            min = arr[i];
+}
+
+    printf("Max: %d\nMin: %d", max,min);
+
+
+
+
+
+return 0;
+
+
+
+
+}
