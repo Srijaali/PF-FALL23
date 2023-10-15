c0f0b0c7503275effe84f0bffa151158b949cb36
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-10-15 19:59:55 +0500

    Create q6.c

---
+++ labs/lab7/q6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    const int x = 100;
+    int freq[100] = {0};
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    int arr[n]; 
+
+    printf("Enter the elements of the array:\n");
+    fflush(stdin);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+
+    for (i = 0; i < x; i++) {
+        if (freq[i] != 0) {
+            printf("Frequency of %d is %d\n", i, freq[i]);
+        }
+    }
+
+    return 0;
+}
