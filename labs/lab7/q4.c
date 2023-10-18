e37b761b3d54bc5b05a1bb7772f20ecd9b24dd1e
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-10-18 20:55:19 +0500

    Create q4.c

---
+++ labs/lab7/q4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int symmetric(int arr[][3], int arr2[][3]){
+            
+            for (int i=0; i<3;i++){
+                for (int j=0;j<3;j++){
+                    arr2[i][j]=arr[j][i];
+                }
+            }
+            
+            for (int i=0; i<3;i++){
+                for (int j=0;j<3;j++){
+                    if(arr[i][j]!=arr2[i][j])
+                        return 0;
+                }
+            }
+            return 1;
+}
+
+int main(){
+    int arr[3][3]={{1, 2, 3}, {2, 4, 5}, {3, 5, 6}};
+    int arr2[3][3];
+    if (symmetric(arr, arr2)==0)
+    printf("Not symmetric");
+    else 
+    printf("symmetric");
+}
