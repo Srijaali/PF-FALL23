4cd9b7c6313f9c8a505dd5d3912b83a974334d5b
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-10-31 10:21:50 +0500

    Update recursion q1.c

--- labs/lab09/recursion q1.c
+++ labs/lab09/recursion q1.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
-void factorial(int n){
+int factorial(int n){
 	if(n==1){
-		printf("the factorial is 1");
+		return 1;
 	}else{
-	
-		int	fact=n*(n-1);
-		//factorial(fact);
-		printf("%d", fact);
-
-}
-	
-		
+	return n*(factorial(n-1));
+   }
 }
 
 
 
 int main(){
-		int n,i;
+		int n;
 		int fact;
 		printf("Enter the value of n: ");
 		scanf("%d", &n);
 		
-		factorial(n);
-	
-	
-	
+	printf("%d", factorial(n));	
 	return 0;
 }
