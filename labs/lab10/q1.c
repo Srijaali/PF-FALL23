ea55f4509828f1a249bcc3464c6520080c983ddb
Srijaali <142867637+Srijaali@users.noreply.github.com>
2023-11-21 21:46:38 +0500

    Create q1.c

---
+++ labs/lab10/q1.c
@@ -0,0 +1,6 @@
+/*
+ * Creator: Syeda Rija Ali
+ * Date: 17/11/23
+ * Description: Creating a universal array print function that
+ *              makes use of pointer arithmetics and variable casting
+ */
